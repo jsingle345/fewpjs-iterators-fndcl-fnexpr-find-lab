124ac7a2e75f9b14d36d226309ecd4061d1a986a
jsingle345 <jsingle345@gmail.com>
2020-11-02 14:56:25 -0600

    commit tree af41d7df73e882a47b2277368de902117bfcf699

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+    let result = record.find(season => season.result === 'W')
+      return !!result ? result.year : undefined
+}
